83140a0fbea87e2c03c0f1a1daf73d69390ea2a6
Xiaofan Li <xli@edbr.uk>
2018-10-09 01:12:20 +0100

    CSC-14 Add support for 3D biomolecular display

--- tasks.py
+++ tasks.py
@@ -47,13 +47,11 @@ def build(ctx, env_name=env_name, kernel=True):
     ctx.run("""
         {0!s} activate {1!s} &&
         jupyter labextension install @jupyterlab/fasta-extension@0.16 --no-build &&
-        jupyter labextension install @jupyterlab/geojson-extension@0.16 --no-build &&
-        jupyter labextension install @jupyterlab/google-drive@0.12 --no-build &&
-        jupyter labextension install @jupyterlab/plotly-extension@0.16 --no-build &&
         jupyter labextension install @jupyter-widgets/jupyterlab-manager@0.35 --no-build &&
         jupyter labextension install bqplot@0.3.6 --no-build &&
         jupyter labextension install jupyter-matplotlib@0.3.0 --no-build &&
-        jupyter labextension install jupyterlab_bokeh &&
+        jupyter labextension install jupyterlab_bokeh --no-build &&
+		jupyter labextension install nglview-js-widgets &&
         jupyter lab clean && jupyter lab build
         """.format(source, env_name).strip().replace('\n', ''))
     if kernel:
